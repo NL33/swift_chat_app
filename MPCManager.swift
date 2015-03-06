957fc1a3aa4eea2a71c269bbacf728e1a1e34b50
NL33 <stkellman@gmail.com>
2015-03-06 13:04:20 -0500

    first commit

---
+++ MPCManager.swift
@@ -0,0 +1,13 @@
+//
+//  MPCManager.swift
+//  MPCRevisited
+//
+//  Created by Sean on 3/6/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class MPCManager: NSObject {
+   
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# swift_chat_app
